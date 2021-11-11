b6d39b26dde180027ee2279421e00f5013f51a63
Tomasz Koba <tomaszkoba@outlook.com>
2021-11-11 11:50:07 +0100

    Initial Commit

---
+++ WelcomeSwiftUI/WelcomeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeSwiftUIApp.swift
+//  WelcomeSwiftUI
+//
+//  Created by Tomasz Koba on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
